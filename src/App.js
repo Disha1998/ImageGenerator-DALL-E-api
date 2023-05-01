74e9848e198fd743bf130ff5c06db7d5fecd1289
Disha Sathavara <dishasathavara1998@gmail.com>
2023-05-01 09:43:15 +0530

    react-dall-e api image prompt

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import ImageGenerator from './component/ImageGenerator';
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <header className="App-header">
+    
+       <ImageGenerator />
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
